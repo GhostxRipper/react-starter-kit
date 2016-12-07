6ec0abf756a6db8764f7034825c7773ae8f5c4bc
Yann Amsellem <yann_ams@icloud.com>
2016-12-07 15:51:50 +0100

    Rect router v4 establishement

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router, Match, Miss, Link } from 'react-router'
 
-export default () => (<div>Test</div>)
+export default () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/yolo">Yolo</Link></li>
+        <li><Link to="/toto">Toto</Link></li>
+      </ul>
+      <hr />
+      <Match exactly pattern="/" render={() => (<div>Home</div>)} />
+      <Match pattern="/yolo" render={() => (<div>Yolo</div>)} />
+      <Match pattern="/toto" render={() => (<div>Toto</div>)} />
+      <Miss
+        render={({ location }) => (
+          <div>Nothing matched {location.pathname}.</div>
+        )}
+      />
+    </div>
+  </Router>
+)
